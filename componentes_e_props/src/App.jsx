f674918f05f916d6b2892bcfa3904740ab355f37
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-25 16:39:14 -0300

    class 13 - exercise 2: using props solution

--- componentes_e_props/src/App.jsx
+++ componentes_e_props/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import HelloName from './components/HelloName';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <>
+      <h2>1 - Criando um componente simples</h2>
+      
       <HelloWorld />
+      
+      <h2>2 - Utilizando props</h2>
+      
+      <HelloName name={'Leonardo'} />
     </>
   );
 }
---
+++ componentes_e_props/src/components/HelloName.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+const HelloName = ({name}) => {
+    return (
+        <p>Hello {name}!</p>
+    );
+};
+
+export default HelloName;
